62c669052ad849071ba20e1f52ceaa75b413a5aa
jacky-hello-cck <69745867+jacky-hello-cck@users.noreply.github.com>
2020-09-08 10:38:52 +0000

    Add files via upload
    You can compile the file via javac <filename> in command prompt or editor.

---
+++ Paint.java
@@ -0,0 +1,25 @@
+/*
+ * Paint.java
+ *  
+ */
+
+
+public class Paint {
+	
+	abstract class GraphicObject {
+    
+    int x, y;
+    
+    void moveTo(int newX, int newY) {
+       
+    }
+    
+    abstract void draw();
+    abstract void resize();
+ }
+	
+	public static void main (String args[]) {
+		
+	}
+}
+
